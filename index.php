b11e150ded08a8bf0b306f62d7974347a020e489
Fikkung <125670574+Fikkung@users.noreply.github.com>
2023-08-11 17:29:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -66,9 +66,10 @@ if ($result->num_rows > 0) {
 <html>
 <head>
     <title>ระบบการจัดการสินค้า</title>
-    <link rel="stylesheet" href="ProJ/style.css">
-    
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair:wght@300&display=swap" rel="stylesheet">
 </head>
 <body class="background-image">
 
